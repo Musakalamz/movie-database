f02dafc8f16823187d37803595ee6fd796221ff1
Musakalamz <ogunsolumusa@gmail.com>
2025-12-01 17:38:54 +0100

    chore: creating the home structure and its routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 function App() {
-  return <h1 className="text-blue-700 text-9xl">Movie_Database</h1>;
+  return (
+    <div className="min-h-screen">
+      <header className="bg-white shadow-sm">
+        <div className="container mx-auto px-4 py-4 flex items-center gap-4">
+          <span className="text-xl font-semibold ">Movie</span>
+          <span className="text-gray-500">Movie Database</span>
+        </div>
+      </header>
+
+      <main className="container mx-auto px-4 py-6">{/* Searchbar */}</main>
+
+      <footer className="container mx-auto px-4 py-8  text-sm text-gray-500">
+        2025
+      </footer>
+    </div>
+  );
 }
 
 export default App;
